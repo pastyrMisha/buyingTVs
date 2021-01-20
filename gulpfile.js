d5ad5422aba765e11a599c1e0d2b27a4011dac60
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-01-20 11:40:17 +0300

    per-start

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 
 function style() {
-    return gulp.src('./scss//*.scss')
+    return gulp.src('./scss/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('./css'))
         .pipe(browserSync.stream())
