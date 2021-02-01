e027b3bc758d617e6093598440dc105611a085a7
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-02-01 15:02:24 +0300

    validation js

--- js/script.js
+++ js/script.js
@@ -8,8 +8,26 @@ document.addEventListener('DOMContentLoaded', function () {
         e.preventDefault();
         let error = formValidate(form);
 
+        let formData = new FormData(form);
+
         if (error === 0) {
-                
+            form.classList.add('_sending');
+
+            let response = await fetch('sendmail.php', {
+                method: 'POST',
+                body: formData
+            });
+            if (response.ok) {
+                let result = await response.json();
+                alert(result.message);
+                form.reset();
+                form.classList.remove('_sending');
+            } else {
+                alert('Ошибка');
+                form.classList.remove('_sending');
+
+            }
+
         } else {
             alert('Заполните обязательные поля');
         }
