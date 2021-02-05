6eee08a5f6a00ac8953b5ea9a895d769737f26c7
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-02-05 21:08:52 +0300

    php mailer done

--- sendmail.php
+++ sendmail.php
@@ -16,5 +16,32 @@
     $mail->addAddress('konkurent.msg@mail.ru');
     // Тема письма
     $mail->Subject = 'Заявка на скупку телевизора с сайта Скупка TV';
+    // Тело письма
+    $body = '<h1>Заявка на покупку ТВ</h1>';
 
+    if(trim(!empty($_POST['name']))){
+        $body.='<p><strong>Модель ТВ:</strong> '.$_POST['name'].'</p>';
+    }
     
+    if(trim(!empty($_POST['price']))){
+        $body.='<p><strong>Желаемая цена:</strong> '.$_POST['price'].'</p>';
+    }
+    
+    if(trim(!empty($_POST['tel']))){
+        $body.='<p><strong>Ваш телефон:</strong> '.$_POST['tel'].'</p>';
+    }
+    
+    $mail->Body = $body;
+
+    // Отправляем
+    if (!$mail->send()) {
+        $message = 'Ошибка';
+    } else {
+        $message = 'Данные отправлены!';
+    }
+
+    $response = ['message' => $message];
+
+    header('Content-type: application/json');
+    echo json_encode($response);
+?>
