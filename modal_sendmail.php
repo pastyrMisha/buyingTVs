e56a9d79bea16f427ea15a265c079a26c6c24f5f
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-02-10 23:53:57 +0300

    php

--- modal_sendmail.php
+++ modal_sendmail.php
@@ -13,8 +13,8 @@
     // От кого письмо
     $mail->setFrom('support@skuptv.ru', 'Скупка TV');
     // Кому отправить
-    $mail->addAddress('SkKonkurent@yandex.ru');
-    $mail->addAddress('SkTurner@yandex.ru');
+    $mail->addAddress('skupka-tv@mail.ru');
+    $mail->addAddress('skkonkurent@yandex.ru');
     // Тема письма
     $mail->Subject = 'Заявка на обратный звонок';
 
